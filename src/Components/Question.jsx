3813580a0fb478766db5d929611daf2fe08e8c30
liadi-ahmed <aoliadi1998@gmail.com>
2022-12-24 22:56:06 +0100

    questions and choices now render correctly! 🚀

--- src/Components/Question.jsx
+++ src/Components/Question.jsx
@@ -1,5 +1,5 @@
 import question from "../css/question.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const questions = [
   {
@@ -39,34 +39,88 @@ const questions = [
     isNiche: false,
   },
 ];
+// const MAX_QUESTIONS = 3;
+// const CORRECT_SCORE = 10;
 
 function Question() {
+  // const [totalScore, setTotalScore] = useState(0);
+  const [questionCounter, setQuestionCounter] = useState(null);
+  const [currentQuestion, setCurrentQuestion] = useState(null);
+  // let availableQuestions = [];
+  const [availableQuestions, setAvailableQuestions] = useState(null);
+  // let answerToCurrentQuestion = "";
+
+  const getAllQuestions = () => {
+    setAvailableQuestions(questions);
+  };
+
+  const chooseCurrentQuestion = () => {
+    const questionToDisplay = Math.floor(
+      Math.random() * availableQuestions.length
+    );
+    setCurrentQuestion([availableQuestions[questionToDisplay]]);
+    setQuestionCounter((prev) => prev + 1);
+
+    // setAvailableQuestions((prevQuestionArr) =>
+    //   prevQuestionArr.splice(questionToDisplay, 1)
+    // );
+  };
+
+  useEffect(() => {
+    getAllQuestions();
+
+    return () => {};
+  }, []);
+
   return (
     <>
-      <section className={question.container}>
-        <h2 className={question.question}>
+      <button onClick={chooseCurrentQuestion}>Start</button>
+      {currentQuestion?.map((obj, id) => {
+        const options = [...obj.incorrectAnswers, obj.correctAnswer];
+        const optionLabels = ["a", "b", "c", "d"];
+
+        return (
+          <section key={id} className={question.container}>
+            <h2 className={question.question}>{obj.question}</h2>
+            <ul className={question.options}>
+              {options.map((option, id) => {
+                return (
+                  <li
+                    key={id}
+                    className={question.option}
+                    data-option={optionLabels[id]}
+                  >
+                    <p className={question.option_label}>{optionLabels[id]}</p>
+                    <p className={question.option_text}>{option}</p>
+                  </li>
+                );
+              })}
+            </ul>
+          </section>
+        );
+      })}
+      {/* <h2 className={question.question}>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis,
           asperiores!
         </h2>
-        <ul className={question.options}>
-          <li className={question.option} data-option="A">
-            <p className={question.option_label}>a</p>
-            <p className={question.option_text}>option</p>
-          </li>
-          <li className={question.option}>
-            <p className={question.option_label}>b</p>
-            <p className={question.option_text}>option</p>
-          </li>
-          <li className={question.option}>
-            <p className={question.option_label}>c</p>
-            <p className={question.option_text}>option</p>
-          </li>
-          <li className={question.option}>
-            <p className={question.option_label}>d</p>
-            <p className={question.option_text}>option</p>
-          </li>
-        </ul>
-      </section>
+      <ul className={question.options}>
+        <li className={question.option} data-option="A">
+          <p className={question.option_label}>a</p>
+          <p className={question.option_text}>option</p>
+        </li>
+        <li className={question.option}>
+          <p className={question.option_label}>b</p>
+          <p className={question.option_text}>option</p>
+        </li>
+        <li className={question.option}>
+          <p className={question.option_label}>c</p>
+          <p className={question.option_text}>option</p>
+        </li>
+        <li className={question.option}>
+          <p className={question.option_label}>d</p>
+          <p className={question.option_text}>option</p>
+        </li>
+      </ul> */}
     </>
   );
 }
