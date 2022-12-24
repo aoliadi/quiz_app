b118ad008420e9f5bac7cebbcb61b5b9eaa6948b
liadi-ahmed <aoliadi1998@gmail.com>
2022-12-24 20:50:40 +0100

    mock questions

--- src/Components/Question.jsx
+++ src/Components/Question.jsx
@@ -1,4 +1,44 @@
 import question from "../css/question.module.css";
+import { useState } from "react";
+
+const questions = [
+  {
+    category: "Sport & Leisure",
+    id: "622a1c347cc59eab6f94f86d",
+    correctAnswer: "Cricket",
+    incorrectAnswers: ["Motor Racing", "Gymnastics", "Basketball"],
+    question: "With which sport is Sir Gary Sobers associated?",
+    tags: ["people", "sport"],
+    type: "Multiple Choice",
+    difficulty: "hard",
+    regions: [],
+    isNiche: false,
+  },
+  {
+    category: "Sport & Leisure",
+    id: "62417d480f96c4efe8d773b7",
+    correctAnswer: "Milwaukee Bucks",
+    incorrectAnswers: ["Milwaukee Heat", "Milwaukee Reds", "Milwaukee Pirates"],
+    question: "Which of these is a basketball team based in Milwaukee?",
+    tags: ["sport"],
+    type: "Multiple Choice",
+    difficulty: "hard",
+    regions: [],
+    isNiche: false,
+  },
+  {
+    category: "Sport & Leisure",
+    id: "6242e860a22452794eaef217",
+    correctAnswer: "The Lions",
+    incorrectAnswers: ["The Boro", "The Tigers", "The Magpies"],
+    question: "What is the nickname of the English football team Millwall?",
+    tags: ["sport"],
+    type: "Multiple Choice",
+    difficulty: "hard",
+    regions: [],
+    isNiche: false,
+  },
+];
 
 function Question() {
   return (
